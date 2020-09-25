b80ac50f515d2f188e1cdf6cbb7c33a31fb5508a
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-09-25 22:34:35 +0900

    Initial commit.

---
+++ index.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         CafeCoderStandingsColorizer
+// @namespace    https://makutamoto.com/
+// @version      0.2
+// @description  Colorize the CafeCoder standings page.
+// @author       Makutamoto
+// @match        https://cafecoder.top/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const colors = ['gray', 'brown', 'green', 'cyan', 'blue', 'yellow', 'orange', 'red'];
+    const root = document.body;
+    const options = {
+        subtree: true,
+        childList: true,
+    };
+    const observer = new MutationObserver((records) => {
+        for(const record of records) {
+            for(const node of record.addedNodes) {
+                if(node.tagName === 'DIV') {
+                    const users = node.querySelectorAll("table.standings__table tbody tr th");
+                    if(users.length > 0) {
+                        for(let user of users) {
+                            fetch(`https://atcoder-badges.now.sh/api/rating/${user.innerText}`).then((data) => {
+                                data.json().then((data) => {
+                                    const rating = data.rating === null ? null : (data.rating.atcoder === null ? null : data.rating.atcoder);
+                                    if(rating) user.style.color = colors[Math.floor(rating / 400)];
+                                })
+                            });
+                        }
+                    }
+                }
+            }
+        }
+    });
+    observer.observe(root, options);
+})();
